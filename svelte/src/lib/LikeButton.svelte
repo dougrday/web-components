8c15cb872ca8c2022f3d25bac1aa5855ddd7cfca
Doug Day <dday@mastercontrol.com>
2022-02-23 19:49:31 -0700

    Figured out input/output for component, along with RxJS integration (surprisingly awesome)

--- svelte/src/lib/LikeButton.svelte
+++ svelte/src/lib/LikeButton.svelte
@@ -1,15 +1,29 @@
 <!--Add this line to your web component-->
-<svelte:options tag="mwc-like-button" />
+<svelte:options tag="mc-like-button" />
 
 <script lang="ts">
   import "@material/mwc-button";
+  import { BehaviorSubject } from "rxjs";
+  import { map, takeUntil } from "rxjs/operators";
+  import { get_current_component, onDestroy } from "svelte/internal";
 
-  let liked = false;
-  let text = "Like";
+  const liked$ = new BehaviorSubject(false);
+  const likeText = $$props["liketext"] ?? "Like";
+  const unlikeText = $$props["unliketext"] ?? "Unlike";
+  const text$ = liked$.pipe(map((liked) => (liked ? unlikeText : likeText)));
+
   const handleClick = () => {
-    liked = !liked;
-    text = liked ? "Unlike" : "Like";
-  }
+    liked$.next(!$liked$);
+  };
+
+  const component = get_current_component();
+  liked$.subscribe((liked) => {
+    component?.dispatchEvent(new CustomEvent("liked", { detail: liked }));
+  });
+
+  onDestroy(() => {
+    liked$.complete();
+  });
 </script>
 
-<mwc-button on:click={handleClick}>{text}</mwc-button>
+<mwc-button on:click={handleClick}>{$text$}</mwc-button>
